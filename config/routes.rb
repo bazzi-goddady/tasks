ff8930e5eab806c787f33c067b101ed9ea916323
Bruno Azzi <bruno-azzi@hotmail.com>
2023-05-30 22:06:29 -0300

    WIP - add auth to tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  scope defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+  end
+
   root 'tasks#index'
 
   resources :tasks, only: [:index, :show, :create, :update]
+  resource :user, only: [:show, :update]
+  
   # get '/tasks', to: 'tasks#index'
   # get '/tasks/:id', to: 'tasks#show'
   
