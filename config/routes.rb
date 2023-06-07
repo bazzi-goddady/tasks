eca949cd8c66b512ff6811ff1159c721f51cd2c2
Bruno Azzi <bruno-azzi@hotmail.com>
2023-06-06 23:08:43 -0300

    added sign_in and sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+    registrations: :registrations,
+    sessions: :sessions
+  }
+
   root 'tasks#index'
 
   resources :tasks, only: [:index, :show, :create, :update]
